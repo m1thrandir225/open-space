e0b21fd24774876b66a3c9c42db9fb3288e55a19
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-10-07 18:54:18 +0200

    sign-in poubavko

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from "next/headers";
+import LogoutButton from "@/components/LogoutButton";
+import {Button} from "@/components/ui/button";
 
 export default async function Home() {
   const supabase = createServerComponentClient({cookies});
 
-  const {data, error} = await supabase.from("tags").select("*");
+  const {
+    data: {session},
+    error,
+  } = await supabase.auth.getSession();
+
+  console.log(session?.user);
 
   return (
-    <div>
-      {data?.map((tag) => (
-        <h1 key={tag.id}> {tag.name}</h1>
-      ))}
-    </div>
+    <div>{session?.user ? <LogoutButton /> : <Button> Login </Button>}</div>
   );
 }
