30204a911f9d58307f93e5d1b9be6cb9414c24e9
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-10-08 07:47:06 +0200

    dynamic force on server rendered pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,24 +2,28 @@ import React, {Suspense} from "react";
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
-import { cookies } from "next/headers";
-import { redirect} from "next/navigation";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {Database} from "@/lib/types/db";
 import DashboardPage from "@/components/pages/Dashboard";
 
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
-    const supabase = createServerComponentClient<Database>({ cookies })
+  const supabase = createServerComponentClient<Database>({cookies});
 
-    const { data : { session }, error} = await supabase.auth.getSession();
+  const {
+    data: {session},
+    error,
+  } = await supabase.auth.getSession();
 
-    if (error || !session) {
-        redirect("/sign-in")
-    }
-    return (
-        <Suspense fallback={<div> Loading... </div>}>
-            <DashboardPage user={session.user} />
-        </Suspense>
-    );
+  if (error || !session) {
+    redirect("/sign-in");
+  }
+  return (
+    <Suspense fallback={<div> Loading... </div>}>
+      <DashboardPage user={session.user} />
+    </Suspense>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from "next/headers";
 import LogoutButton from "@/components/LogoutButton";
 import {Button} from "@/components/ui/button";
-import {useQuery} from "@tanstack/react-query";
+
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const supabase = createServerComponentClient({cookies});
