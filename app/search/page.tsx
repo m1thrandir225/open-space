952b0a67b5b2133e54c04b66cfe9869f2da5cf72
Marko Ilioski <markoilioski21@gmail.com>
2023-10-08 11:50:52 +0200

    Made a template for search.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -92,51 +92,51 @@ const sortOptions = [
     {name: 'Price: High to Low', href: '#', current: false},
 ]
 
-//
-// const subCategories = [
-//     { name: 'Totes', href: '#' },
-//     { name: 'Backpacks', href: '#' },
-//     { name: 'Travel Bags', href: '#' },
-//     { name: 'Hip Bags', href: '#' },
-//     { name: 'Laptop Sleeves', href: '#' },
-// ]
-const filters = [
-    {
-        id: 'tags',
-        name: 'Tags',
-        options: [
-            {value: 'white',  checked: false},
-            {value: 'beige',  checked: false},
-            {value: 'blue',  checked: true},
-            {value: 'brown', checked: false},
-            {value: 'green', checked: false},
-            {value: 'purple', checked: false},
-        ],
-    }//,
-    // {
-    //     id: 'category',
-    //     name: 'Category',
-    //     options: [
-    //         {value: 'new-arrivals', label: 'New Arrivals', checked: false},
-    //         {value: 'sale', label: 'Sale', checked: false},
-    //         {value: 'travel', label: 'Travel', checked: true},
-    //         {value: 'organization', label: 'Organization', checked: false},
-    //         {value: 'accessories', label: 'Accessories', checked: false},
-    //     ],
-    // },
-    // {
-    //     id: 'size',
-    //     name: 'Size',
-    //     options: [
-    //         {value: '2l', label: '2L', checked: false},
-    //         {value: '6l', label: '6L', checked: false},
-    //         {value: '12l', label: '12L', checked: false},
-    //         {value: '18l', label: '18L', checked: false},
-    //         {value: '20l', label: '20L', checked: false},
-    //         {value: '40l', label: '40L', checked: true},
-    //     ],
-    // },
+
+const subCategories = [
+    {name: 'Totes', href: '#'},
+    {name: 'Backpacks', href: '#'},
+    {name: 'Travel Bags', href: '#'},
+    {name: 'Hip Bags', href: '#'},
+    {name: 'Laptop Sleeves', href: '#'},
 ]
+// const filters = [
+//     {
+//         id: 'tags',
+//         name: 'Tags',
+//         options: [
+//             {value: 'white',  checked: false},
+//             {value: 'beige',  checked: false},
+//             {value: 'blue',  checked: true},
+//             {value: 'brown', checked: false},
+//             {value: 'green', checked: false},
+//             {value: 'purple', checked: false},
+//         ],
+//     }//,
+// {
+//     id: 'category',
+//     name: 'Category',
+//     options: [
+//         {value: 'new-arrivals', label: 'New Arrivals', checked: false},
+//         {value: 'sale', label: 'Sale', checked: false},
+//         {value: 'travel', label: 'Travel', checked: true},
+//         {value: 'organization', label: 'Organization', checked: false},
+//         {value: 'accessories', label: 'Accessories', checked: false},
+//     ],
+// },
+// {
+//     id: 'size',
+//     name: 'Size',
+//     options: [
+//         {value: '2l', label: '2L', checked: false},
+//         {value: '6l', label: '6L', checked: false},
+//         {value: '12l', label: '12L', checked: false},
+//         {value: '18l', label: '18L', checked: false},
+//         {value: '20l', label: '20L', checked: false},
+//         {value: '40l', label: '40L', checked: true},
+//     ],
+// },
+//]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -148,7 +148,7 @@ export default function Seach() {
 
     const supabase = createClientComponentClient()
 
-    const categories = useQuery(['subcategories', filterOption], async () => {
+    const categories = useQuery(['categories', filterOption], async () => {
         const {data, error} = await supabase.from("categories").select("name")
 
         if (error) {
@@ -158,8 +158,8 @@ export default function Seach() {
         return data;
     })
 
-    const tags = useQuery(['subcategories', filterOption], async () => {
-        const {data, error} = await supabase.from("categories").select("name")
+    const tags = useQuery(['tags', filterOption], async () => {
+        const {data, error} = await supabase.from("tags").select("name")
 
         if (error) {
             throw error
@@ -230,52 +230,50 @@ export default function Seach() {
                                             ))}
                                         </ul>
 
-                                        {filters.map((section) => (
-                                            <Disclosure as="div" key={section.id}
-                                                        className="border-t border-gray-200 px-4 py-6">
-                                                {({open}) => (
-                                                    <>
-                                                        <h3 className="-mx-2 -my-3 flow-root">
-                                                            <Disclosure.Button
-                                                                className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
+                                        <Disclosure as="div" key="tags"
+                                                    className="border-t border-gray-200 px-4 py-6">
+                                            {({open}) => (
+                                                <>
+                                                    <h3 className="-mx-2 -my-3 flow-root">
+                                                        <Disclosure.Button
+                                                            className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
                                                                     <span
-                                                                        className="font-medium text-gray-900">{section.name}</span>
-                                                                <span className="ml-6 flex items-center">
+                                                                        className="font-medium text-gray-900">Tags</span>
+                                                            <span className="ml-6 flex items-center">
                                   {open ? (
                                       <MinusIcon className="h-5 w-5" aria-hidden="true"/>
                                   ) : (
                                       <PlusIcon className="h-5 w-5" aria-hidden="true"/>
                                   )}
                                 </span>
-                                                            </Disclosure.Button>
-                                                        </h3>
-                                                        <Disclosure.Panel className="pt-6">
-                                                            <div className="space-y-6">
-                                                                {section.options.map((option, optionIdx) => (
-                                                                    <div key={option.value}
-                                                                         className="flex items-center">
-                                                                        <input
-                                                                            id={`filter-mobile-${section.id}-${optionIdx}`}
-                                                                            name={`${section.id}[]`}
-                                                                            defaultValue={option.value}
-                                                                            type="checkbox"
-                                                                            defaultChecked={option.checked}
-                                                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                                        />
-                                                                        <label
-                                                                            htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                                                            className="ml-3 min-w-0 flex-1 text-gray-500"
-                                                                        >
-                                                                            {option.value}
-                                                                        </label>
-                                                                    </div>
-                                                                ))}
-                                                            </div>
-                                                        </Disclosure.Panel>
-                                                    </>
-                                                )}
-                                            </Disclosure>
-                                        ))}
+                                                        </Disclosure.Button>
+                                                    </h3>
+                                                    <Disclosure.Panel className="pt-6">
+                                                        <div className="space-y-6">
+                                                            {tags.data && tags.data.map((option, optionIdx) => (
+                                                                <div key={option.name}
+                                                                     className="flex items-center">
+                                                                    <input
+                                                                        id="tags"
+                                                                        name="tags"
+                                                                        defaultValue={option.name}
+                                                                        type="checkbox"
+                                                                        defaultChecked={false}
+                                                                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                                                    />
+                                                                    <label
+                                                                        htmlFor="tags"
+                                                                        className="ml-3 min-w-0 flex-1 text-gray-500"
+                                                                    >
+                                                                        {option.name}
+                                                                    </label>
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                    </Disclosure.Panel>
+                                                </>
+                                            )}
+                                        </Disclosure>
                                     </form>
                                 </Dialog.Panel>
                             </Transition.Child>
@@ -367,50 +365,48 @@ export default function Seach() {
                                     ))}
                                 </ul>
 
-                                {filters.map((section) => (
-                                    <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
-                                        {({open}) => (
-                                            <>
-                                                <h3 className="-my-3 flow-root">
-                                                    <Disclosure.Button
-                                                        className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                                <Disclosure as="div" key="tags" className="border-b border-gray-200 py-6">
+                                    {({open}) => (
+                                        <>
+                                            <h3 className="-my-3 flow-root">
+                                                <Disclosure.Button
+                                                    className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                                                             <span
-                                                                className="font-medium text-gray-900">{section.name}</span>
-                                                        <span className="ml-6 flex items-center">
+                                                                className="font-medium text-gray-900">Tags</span>
+                                                    <span className="ml-6 flex items-center">
                               {open ? (
                                   <MinusIcon className="h-5 w-5" aria-hidden="true"/>
                               ) : (
                                   <PlusIcon className="h-5 w-5" aria-hidden="true"/>
                               )}
                             </span>
-                                                    </Disclosure.Button>
-                                                </h3>
-                                                <Disclosure.Panel className="pt-6">
-                                                    <div className="space-y-4">
-                                                        {section.options.map((option, optionIdx) => (
-                                                            <div key={option.value} className="flex items-center">
-                                                                <input
-                                                                    id={`filter-${section.id}-${optionIdx}`}
-                                                                    name={`${section.id}[]`}
-                                                                    defaultValue={option.value}
-                                                                    type="checkbox"
-                                                                    defaultChecked={option.checked}
-                                                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                                />
-                                                                <label
-                                                                    htmlFor={`filter-${section.id}-${optionIdx}`}
-                                                                    className="ml-3 text-sm text-gray-600"
-                                                                >
-                                                                    {option.value}
-                                                                </label>
-                                                            </div>
-                                                        ))}
-                                                    </div>
-                                                </Disclosure.Panel>
-                                            </>
-                                        )}
-                                    </Disclosure>
-                                ))}
+                                                </Disclosure.Button>
+                                            </h3>
+                                            <Disclosure.Panel className="pt-6">
+                                                <div className="space-y-4">
+                                                    {tags.data && tags.data.map((option) => (
+                                                        <div key={option.name} className="flex items-center">
+                                                            <input
+                                                                id="tags"
+                                                                name="tags"
+                                                                defaultValue={option.name}
+                                                                type="checkbox"
+                                                                defaultChecked={false}
+                                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                                            />
+                                                            <label
+                                                                htmlFor={`filter-tags-`}
+                                                                className="ml-3 text-sm text-gray-600"
+                                                            >
+                                                                {option.name}
+                                                            </label>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            </Disclosure.Panel>
+                                        </>
+                                    )}
+                                </Disclosure>
                             </form>
 
                             {/* Product grid */}
